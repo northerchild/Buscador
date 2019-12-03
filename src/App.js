242bee63c74a5294713292344106f29b82facec9
northerchild <osarus-13@hotmail.com>
2019-12-03 12:36:36 -0500

    Archivos base

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app container">
+       <div className="jumbotron">
+         <p className="lead text-center">Buscador de Imágenes</p>
+       </div>
+       <div className="row justify-content-center">
+         
+       </div>
+    </div>
+  );
+}
+
+export default App;
