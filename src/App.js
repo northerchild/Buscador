9e9a694a0ad2859b15245a09e0c91accfb9a17a9
northerchild <osarus-13@hotmail.com>
2019-12-03 18:56:59 -0500

    Se muestran las imagenes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React,{useState,useEffect} from 'react';
-import Buscador from './components/Buscador'
+import Buscador from './components/Buscador';
+import ListadoImagenes from './components/ListadoImagenes';
 function App() {
   const [busqueda, guardarBusqueda] = useState('');
+  const [imagenes, guardarImagenes] = useState([]);
   useEffect(()=>{
     const consultarApi = async ()=>{
       const imagenesPorPagina = 30;
@@ -9,7 +11,7 @@ function App() {
       const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
-      console.log(resultado);
+      guardarImagenes(resultado.hits)
     }
     consultarApi()
   },[busqueda])
@@ -20,7 +22,7 @@ function App() {
          <Buscador guardarBusqueda={guardarBusqueda}/>
        </div>
        <div className="row justify-content-center">
-
+          <ListadoImagenes imagenes={imagenes}/>
        </div>
     </div>
   );
