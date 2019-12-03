118fe9cb2cc70e70016e1b90d30c517254c66e69
northerchild <osarus-13@hotmail.com>
2019-12-03 13:25:29 -0500

    Se crea la validacion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
+import Buscador from './components/Buscador'
 function App() {
   return (
     <div className="app container">
        <div className="jumbotron">
          <p className="lead text-center">Buscador de Imágenes</p>
+         <Buscador />
        </div>
        <div className="row justify-content-center">
-         
+
        </div>
     </div>
   );
---
+++ src/components/Buscador.js
@@ -0,0 +1,45 @@
+import React,{useState} from 'react'
+import Error from './Error';
+
+function Buscador(){
+
+    const [terminoBusqueda, guardarterminoBusqueda] = useState('');
+    const [error, guardarError] = useState(false);
+    const buscarImagen = e =>{
+        e.preventDefault();
+        //validar
+        if(terminoBusqueda === '' ){
+            guardarError(true);
+            return;
+        }
+        //Enviar el termino hacia el componente principal
+        guardarError(false);
+    }
+
+    return(
+        <form 
+            onSubmit={buscarImagen}
+        >
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input 
+                    type="text"
+                    className="form-control from-control-lg"
+                    placeholder="Busca una imagen, ejemplo: Futbol o Café"
+                    onChange={e=> guardarterminoBusqueda(e.target.value)}/>
+                </div>
+
+                <div className="form-group col-md-3">
+                    <input 
+                    type="submit"
+                    className="btn btn-lg btn-danger btn-block"
+                    value="Buscar"/>
+                </div>
+            </div>
+            {(error) ? <Error mensaje='El campo no puede estar vacio' /> : null}
+        </form>
+    )
+
+}
+
+export default Buscador;
